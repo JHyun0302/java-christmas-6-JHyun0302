40936cf737223edfe7f86eefcfaad385a4457b9b
JHyun0302 <wogustkfkd11@gmail.com>
2023-11-13 23:45:55 +0900

    :sparkles: [Feat] 혜택 내역

---
+++ src/main/java/christmas/constant/CheckCondition.java
@@ -0,0 +1,7 @@
+package christmas.constant;
+
+public class CheckCondition {
+    public static int CHECK_GIFT_MENU = 120000;
+    public static int CHECK_APPLY_BENEFIT = 10000;
+    public static int CHRISTMAS = 25;
+}
---
+++ src/main/java/christmas/constant/SalePolicyMoney.java
@@ -0,0 +1,7 @@
+package christmas.constant;
+
+public class SalePolicyMoney {
+    public static int discountPerDessert = 2023;
+    public static int baseDiscount = 1000;
+    public static int increaseDiscountPerDay = 100;
+}
